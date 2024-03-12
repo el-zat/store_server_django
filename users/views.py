d040a77e16ee9800377c6f0f2efd257c5efd0517
Elena Zatykina <elena.zatykina@hexagon.com>
2024-03-12 21:24:04 +0100

    create profile form

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.contrib import auth
 from django.urls import reverse
 
@@ -34,5 +34,14 @@ def registration(request):
 
 
 def profile(request):
-    context = {'title': 'Store - Профиль'}
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {'title': 'Store - Профиль', 'form': form}
     return render(request, 'users/profile.html', context)
