90680e7b148ad98099f12f335d84c041ec684ef4
Elena Zatykina <elena.zatykina@hexagon.com>
2024-03-17 19:59:33 +0100

    class based views for index and products

--- store/urls.py
+++ store/urls.py
@@ -18,12 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from products.views import index
+from products.views import IndexView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('products/', include('products.urls', namespace='products')),
     path('users/', include('users.urls', namespace='users')),
     path('accounts/', include('allauth.urls')),
