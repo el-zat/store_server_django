d0690c4ec8d50a50eae5fc66b2d1d1d7ec4bc80e
Elena Zatykina <elena.zatykina@hexagon.com>
2024-03-21 15:28:10 +0100

    add tests for index page

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+# from django.test import TestCase
+# from django.urls import reverse
+# import os
+# from http import HTTPStatus
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
+#
+# import django
+#
+# django.setup()
+
 
 # Create your tests here.
+# class IndexViewTestCase(TestCase):
+#
+#     def test_view(self):
+#         path = reverse('index')
+#         response = self.client.get(path)
+#         print(response)
+#
+#         self.assertEqual(response.status_code, HTTPStatus.OK)
+#         self.assertEqual(response.context_data['title'], 'Store')
+#
+#         self.assertNotEqual(response.context_data['title'], 'Store-test')
+#
+#         self.assertTemplateUsed(response, 'products/index.html')
+#
+
