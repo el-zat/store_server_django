699e221cac78a6b68bb6ffe331f65fa746848dd3
Elena Zatykina <elena.zatykina@hexagon.com>
2024-03-27 19:41:50 +0100

    setup Stripe

--- orders/urls.py
+++ orders/urls.py
@@ -1,12 +1,13 @@
 
 from django.urls import path
 
-from orders.views import OrderCreateView
+from orders.views import OrderCreateView, SuccessTemplateView, CancelledTemplateView
 
 app_name = 'orders'
 
 
 urlpatterns = [
     path('order-create/', OrderCreateView.as_view(), name='order-create'),
-
+    path('order-success/', SuccessTemplateView.as_view(), name='order-success'),
+    path('order-cancelled/', CancelledTemplateView.as_view(), name='order-cancelled'),
 ]
