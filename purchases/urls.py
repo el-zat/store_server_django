0d88cb2c38002654ef9a23405cf961449c2f01ff
Elena Zatykina <elena.zatykina@hexagon.com>
2024-03-31 20:34:40 +0200

    setup Stripe payment

--- purchases/urls.py
+++ purchases/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from purchases.views import PurchaseCreateView
+from purchases.views import PurchaseCreateView, SuccessTemplateView, CancelledTemplateView
 
-app_name = 'orders'
+app_name = 'purchases'
 
 
 urlpatterns = [
     path('place-order/', PurchaseCreateView.as_view(), name='place-order'),
-
+    path('purchases/', PurchaseCreateView.as_view(), name='purchases'),
+    path('order-success/', SuccessTemplateView.as_view(), name='order-success'),
+    path('order-cancelled/', CancelledTemplateView.as_view(), name='order-cancelled'),
 ]
--- purchases/views.py
+++ purchases/views.py
@@ -2,9 +2,24 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from purchases.forms import PurchaseForm
 from store.common.views import TitleMixin
+import stripe
+from django.urls import reverse_lazy, reverse
+from django.conf import settings
+from django.views.generic.base import TemplateView
+from django.http import HttpResponseRedirect
+from http import HTTPStatus
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
-# Create your views here.
+class SuccessTemplateView(TitleMixin, TemplateView):
+    template_name = 'purchases/success.html'
+    title = 'Successful order'
+
+
+class CancelledTemplateView(TemplateView):
+    template_name = 'purchases/cancelled.html'
+
 
 class PurchaseCreateView(TitleMixin, CreateView):
     template_name = 'purchases/place-order.html'
@@ -16,3 +31,17 @@ class PurchaseCreateView(TitleMixin, CreateView):
         form.instance.initiator = self.request.user
         return super(PurchaseCreateView, self).form_valid(form)
 
+    def post(self, request, *args, **kwargs):
+        super(PurchaseCreateView, self).post(request, *args, **kwargs)
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    'price': 'price_1Oz0v3EZ0aTg4ftyXktQSIXU',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url='{}{}'.format(settings.DOMAIN_NAME, reverse('purchases:order-success')),
+            cancel_url='{}{}'.format(settings.DOMAIN_NAME, reverse('purchases:order-cancelled')),
+        )
+        return HttpResponseRedirect(checkout_session.url, status=HTTPStatus.SEE_OTHER)
