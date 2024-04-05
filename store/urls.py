8f71b3884bcdf83311cb76403039e2b5628a4739
Elena Zatykina <elena.zatykina@hexagon.com>
2024-04-05 12:52:40 +0200

    add authorization and permission

--- store/urls.py
+++ store/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from products.views import IndexView
 from purchases.views import stripe_webhook_view
@@ -32,6 +33,7 @@ urlpatterns = [
     path('purchases/', include('purchases.urls', namespace='purchases')),
     path('webhook/stripe/', stripe_webhook_view, name='stripe_webhook'),
     path('api/', include('api.urls', namespace='api')),
+    path('api-token-auth/', obtain_auth_token)
 ]
 
 if settings.DEBUG:
