30c7d6315420a1d9518f617d1872a531dde36242
Elena Zatykina <elena.zatykina@hexagon.com>
2024-04-05 11:30:45 +0200

    create api for all request methods

--- api/views.py
+++ api/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
 
 from products.models import Product
 from products.serializers import ProductSerializer
 # Create your views here.
 
-class ProductListAPIView(ListAPIView):
+
+class ProductModelViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
