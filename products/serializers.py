560d235819b34b04eb67a28a3a2566550a5f3487
Elena Zatykina <elena.zatykina@hexagon.com>
2024-04-05 09:37:33 +0200

    add categories and pagination in serializer

--- products/serializers.py
+++ products/serializers.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 
 from products.models import Product
 
+
 class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field='name', read_only=True)
+
     class Meta:
         model = Product
-        fields = ('id', 'name', 'description', 'price', 'quantity')
+        fields = ('id', 'name', 'description', 'price', 'quantity', 'category')
